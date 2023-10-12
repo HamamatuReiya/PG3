6815b06980ef01c6887bb7018cd3bfe65813fd82
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-10-12 14:29:27 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 template <typename Type1>
-Type1 add(Type1 a, Type1 b) {
+Type1 Min(Type1 a, Type1 b) {
 	if (a < b) {
 		return a;
 	}
@@ -12,20 +12,19 @@ Type1 add(Type1 a, Type1 b) {
 }
 
 template<>
-char add<char>(char a, char b) {
+char Min<char>(char a, char b) {
 	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
 	return a;
 }
 
 int main() {
 
-	printf("%d\n", add<int>(114, 514));
-	printf("%f\n", add<float>(11.4f, 51.4f));
-	printf("%f\n", add<double>(11.4f, 51.4f));
-	add<char>(114,514);
-	
-	
-
+	char a = 'A';
+	char b = 'B';
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%f\n", Min<double>(11.4f, 51.4f));
+	Min<char>(a, b);
 
 	return 0;
 }
