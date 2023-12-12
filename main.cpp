101f03d1519870d70af60683207ac8dbc40c21ca
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-12-12 10:05:51 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,33 +1,17 @@
 #include<stdio.h>
 
-template <typename Type1>
-Type1 Min(Type1 a, Type1 b) {
-	if (a < b) {
-		return a;
+int SalaryCalculation(int time,int num) {
+	if (time <= 1) {
+		return num;
 	}
-	else
-	{
-		return b;
-	}
-}
-
-template<>
-char Min<char>(char a, char b) {
-	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
-	return a;
+	
+	printf("%d‰~\n", num);
+	return (num + SalaryCalculation(time - 1,num * 2 - 50));
 }
 
 int main() {
-
-	char a = 'A';
-	char b = 'B';
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%f\n", Min<double>(11.4f, 51.4f));
-	Min<char>(a,b);
-	
-	
-
+	int wokeTime = 8;
+	int result = SalaryCalculation(wokeTime,100);
 
 	return 0;
 }
