956a17399c0d03ee79d23528914dc85e7ec4097c
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-02-13 17:06:10 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,50 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-#include <time.h>
-#include <functional>
+#include<stdio.h>
+#include "Enemy.h"
 
 int main() {
+	Enemy* enemy_ = new Enemy;
 
-	unsigned int  currentTime = time(nullptr);
-	srand(currentTime);
-
-	int r = rand();
-	int dice = r % 2 + 0;
-	int number = 0;
-
-	printf("半(0)か丁(1)を選んでください\n");
-	scanf_s("%d", &number);
-	printf("選んだ数字 : %d\n", number);
-
-	std::function<void(void)> dispResult = [=]() {
-		if (dice == 0) {
-			if (number == 0) {
-				printf("丁!　当たり!\n");
-			}
-			else {
-				printf("半!　残念...\n");
-			}
-		}
-
-		if (dice == 1) {
-			if (number == 1) {
-				printf("半!　当たり!\n");
-			}
-			else {
-				printf("丁!　残念...\n");
-			}
-		}
-		};
-
-	std::function<void(int)> setTimeout = [=](int second) {
-
-		Sleep(second * 1000);
-
-		dispResult();
-		};
-
-	setTimeout(3);
+	for (int i = 0; i < 3; i++) {
+		enemy_->Update();
+	}
 
 	return 0;
 }
