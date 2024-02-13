5fd5c47fcdc10c20c29f25a0099b8faf30c6c645
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-02-13 18:39:21 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
-#include<stdio.h>
-#include<thread>
+#include <stdio.h>
+#include <thread>
 
-void Thread1() {
+
+void Print1() {
 	printf("thread1\n");
 }
 
-void Thread2() {
+void Print2() {
 	printf("thread2\n");
 }
 
-void Thread3() {
+void Print3() {
 	printf("thread3\n");
 }
 
 int main() {
-	std::thread th1(Thread1);
-	std::thread th2(Thread2);
-	std::thread th3(Thread3);
 
+	std::thread th1(Print1);
 	th1.join();
+
+	std::thread th2(Print2);
 	th2.join();
+
+	std::thread th3(Print3);
 	th3.join();
 
 	return 0;
+
 }
