20a2861e318aa9b5288c756813620f9414c8fca6
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-02-13 17:10:20 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,30 +1,20 @@
 #include<stdio.h>
-
-template <typename Type1>
-Type1 Min(Type1 a, Type1 b) {
-	if (a < b) {
-		return a;
-	}
-	else
-	{
-		return b;
-	}
-}
-
-template<>
-char Min<char>(char a, char b) {
-	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
-	return a;
-}
+#include "Box.h"
 
 int main() {
+	Box<int, int>n1(10, 5);
+	Box<float, float>n2(3.5f, 2.4f);
+	Box<double, double>n3(7.5, 9.9);
+	Box<int, float>n4(10, 5.0f);
+	Box<int, double>n5(3, 4.0);
+	Box<float, double>n6(15.0f, 18.0);
 
-	char a = 'A';
-	char b = 'B';
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%f\n", Min<double>(11.4f, 51.4f));
-	Min<char>(a, b);
+	printf("%d\n", n1.Min());
+	printf("%f\n", n2.Min());
+	printf("%lf\n", n3.Min());
+	printf("%d\n", n4.Min());
+	printf("%d\n", n5.Min());
+	printf("%f\n", n6.Min());
 
 	return 0;
 }
